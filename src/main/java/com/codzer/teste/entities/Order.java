03c3ad882ebb879e6ef26955df51cf3e66bf9118
PabloCodzer <pablo.cozer@gmail.com>
2024-06-11 17:26:05 -0300

    OrderStatus enum

--- src/main/java/com/codzer/teste/entities/Order.java
+++ src/main/java/com/codzer/teste/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.codzer.teste.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
@@ -22,6 +23,8 @@ public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	private Integer orderStatus;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -38,10 +41,11 @@ public class Order implements Serializable {
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -55,6 +59,20 @@ public class Order implements Serializable {
 
 	public Instant getMoment() {
 		return moment;
+	}
+	
+	
+
+	public OrderStatus getOrderStatus() {
+		return  OrderStatus.valueOf(  orderStatus );
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		
+		if(orderStatus != null)
+		{
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public void setMoment(Instant moment) {
