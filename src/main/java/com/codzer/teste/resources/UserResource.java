86a901989805f9379e67ca2a8795be9e3eb1c235
PabloCodzer <pablo.cozer@gmail.com>
2024-06-20 18:26:31 -0300

    User insert/create

--- src/main/java/com/codzer/teste/resources/UserResource.java
+++ src/main/java/com/codzer/teste/resources/UserResource.java
@@ -1,14 +1,18 @@
 package com.codzer.teste.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.codzer.teste.entities.User;
 import com.codzer.teste.services.UserService;
@@ -32,5 +36,13 @@ public class UserResource {
 	{
 		User obj = service.findById(id);
 		return  ResponseEntity.ok().body( obj );
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody  User obj)
+	{
+		obj = service.insert(obj);
+		URI uri_new_user = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri_new_user).body(obj);
 	}
 }
