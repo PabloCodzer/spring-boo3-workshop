9a17e613565d641f72e93f64fb463f8c88805601
PabloCodzer <pablo.cozer@gmail.com>
2024-06-24 17:32:37 -0300

    Error handler Delete and Update

--- src/main/java/com/codzer/teste/services/UserService.java
+++ src/main/java/com/codzer/teste/services/UserService.java
@@ -4,12 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.codzer.teste.entities.User;
 import com.codzer.teste.repositories.UserRepository;
+import com.codzer.teste.services.exceptions.DatabaseException;
 import com.codzer.teste.services.exceptions.RessourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -37,14 +42,29 @@ public class UserService {
 	
 	public void delete(Long id)
 	{
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) 
+		{
+			throw new RessourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj)
 	{
-		User entity = repository.getReferenceById(id);
-		entity.setName( obj.getName() );
-		entity.setPhone(obj.getPhone());
-		return repository.save( entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			entity.setName( obj.getName() );
+			entity.setPhone(obj.getPhone());
+			return repository.save( entity);
+		}
+		catch (EntityNotFoundException e) 
+		{
+			throw new RessourceNotFoundException(id);
+		}
 	}
 }
